fe1a3c36434a8dfe04fb024560165ca1c0079c35
cristianasir2 <manzcris@gmail.com>
2025-03-05 22:36:45 +0100

    Control de gestión de actividad en LIBRO

--- src/_biblioteca/libro/libro.controller.ts
+++ src/_biblioteca/libro/libro.controller.ts
@@ -1,13 +1,15 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Put } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Put, UseGuards } from '@nestjs/common';
 import { LibroService } from './libro.service';
 import { CreateLibroDto } from './dto/create-libro.dto';
 import { UpdateLibroDto } from './dto/update-libro.dto';
+import { JwtAuthGuard } from '../auth/jwt/jwt.guard';
 
 @Controller('libro')
 export class LibroController {
   constructor(private readonly libroService: LibroService) { }
 
   @Post()
+  @UseGuards(JwtAuthGuard)
   create(@Body() createLibroDto: CreateLibroDto) {
     return this.libroService.create(createLibroDto);
   }
@@ -21,12 +23,12 @@ export class LibroController {
   findOne(@Param('id') id: string) {
     return this.libroService.findOne(+id);
   }
-
+  @UseGuards(JwtAuthGuard)
   @Put(':id')
   update(@Param('id') id: string, @Body() updateLibroDto: UpdateLibroDto) {
     return this.libroService.update(+id, updateLibroDto);
   }
-
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.libroService.remove(+id);
